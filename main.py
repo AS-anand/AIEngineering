3b50d035c78eb16a5fc6fdc5125cbb86b40da895
Anurag Anand <anurag.anand.2pb@gmail.com>
2026-09-25 12:03:17 +0530

    Add HTTP POST and GET requests with error handling

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-import sys
+import httpx
 
-print(sys.version)
-print(sys.executable)
+# response = httpx.get("https://httpbin.org/get")
+
+# print(response.status_code)
+# print(response.headers)
+# print(response.text)
+
+data = {
+    "name": "Anurag",
+    "role": "developer"
+}
+
+response = httpx.post(
+    "https://httpbin.org/post",
+    json=data
+)
+
+print(response.status_code)
+print(response.json())
+
+# Error handling
+
+try:
+    response = httpx.get("https://httpbin.org/get")
+
+    response.raise_for_status()
+
+    data = response.json()
+    print(data)
+
+except httpx.HTTPError as e:
+    print(f"API request failed: {e}")
